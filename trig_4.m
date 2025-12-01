268b06f8c88fc152507aa0b8d276040799aff54a
Yasar Abbas <yasir.abbas42@gmail.com>
2025-12-01 16:11:29 +0800

    Update trig_4.m

--- trig_4.m
+++ trig_4.m
@@ -7,8 +7,8 @@ clear all
 clc
  set(0,'DefaultFigureWindowStyle','docked');
 % set(0,'InitialMagnification','fit');
- path(path,'e:\personal\Research files\MS Research Work\paper1\scenario4\video data set\winterDriveway\input');
-% path(path,'E:\personal\Research files\MS Research Work\paper1\scenario4\video data set\parking\input');
+ path(path,'\scenario4\video data set\winterDriveway\input');
+
 %--------------------------------------------------------------------------
 count_detect=0;
 count_undetect=0;
